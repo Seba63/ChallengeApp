5e6195232a07a1d99f0cffc6e8dabae4de9b7790
Grzeszczak <sebastian.grzeszczak0@gmail.com>
2023-01-26 20:57:18 +0100

    praca domowa 5

---
+++ ChallengeApp/ChallengeApp/praca domowa 4/praca domowa 4/Praca domowa.cs
@@ -0,0 +1,23 @@
+﻿string name = "Jowita";
+string gender = "Female";
+int age = 28;
+
+
+if (name == "Jowita" && gender == "Female" && age == 28)
+{
+    Console.WriteLine("Wszystko się zgadza");
+}
+
+else
+{
+    Console.WriteLine("Lipa");
+}
+
+if (gender == "Female" && age < 30)
+{
+    Console.WriteLine("Kobieta przed 30");
+}
+else if (gender == "Female" && age > 30)
+{
+    Console.WriteLine("Kobieta po 30");
+}
---
+++ ChallengeApp/ChallengeApp/praca domowa 5/praca domowa 5/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Globalization;
+
+int number = 638374;
+string numbersAsString = number.ToString();
+char[] digits = numbersAsString.ToArray();
+
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+foreach (char digit in digits)
+{
+    if (digit == '0')
+    {
+        counter0++;
+    }
+    else if (digit == '1')
+    {
+        counter1++;
+    }
+    else if (digit == '2')
+    {
+        counter2++;
+    }
+    else if (digit == '3')
+    {
+        counter3++;
+    }
+    else if (digit == '4')
+    {
+        counter4++;
+    }
+    else if (digit == '5')
+    {
+        counter5++;
+    }
+    else if (digit == '6')
+    {
+        counter6++;
+    }
+    else if (digit == '7')
+    {
+        counter7++;
+    }
+    else if (digit == '8')
+    {
+        counter8++;
+    }
+    else if (digit == '9')
+    {
+        counter9++;
+    }
+}
+Console.WriteLine($"0 = {counter0}");
+Console.WriteLine($"1 = {counter1}");
+Console.WriteLine($"2 = {counter2}");
+Console.WriteLine($"3 = {counter3}");
+Console.WriteLine($"4 = {counter4}");
+Console.WriteLine($"5 = {counter5}");
+Console.WriteLine($"6 = {counter6}");
+Console.WriteLine($"7 = {counter7}");
+Console.WriteLine($"8 = {counter8}");
+Console.WriteLine($"9 = {counter9}");
